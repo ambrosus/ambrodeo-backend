8aa1f509bc6d5f0229ec95fda37fdb8b7c3f4685
Voktor Stolenets <v.stolenets@inc4.net>
2024-12-24 15:43:44 +0200

    Change: add swagger ui

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import Fastify from "fastify";
 import multipart from "@fastify/multipart";
+import swagger from "@fastify/swagger";
+import { OpenAPIV3 } from "openapi-types";
+import swaggerUi from "@fastify/swagger-ui";
+import fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
 import { handleRPCRequest } from "./handlers";
 import { uploadHandler } from "./uploadHandler";
 import { connectToDB } from "./db";
@@ -7,6 +13,22 @@ import { HOST, PORT, DATABASE_URL, DATABASE } from "./env";
 const fastify = Fastify({
   bodyLimit: 5000000,
   logger: true,
+});
+
+const openApiSpec = yaml.load(
+  fs.readFileSync(path.join(__dirname, "api.yaml"), "utf8"),
+) as OpenAPIV3.Document;
+
+fastify.register(swagger, {
+  openapi: openApiSpec,
+});
+
+fastify.register(swaggerUi, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
 });
 
 fastify.register(multipart);
